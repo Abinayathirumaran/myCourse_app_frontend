ba753d5965d4b7bdb5e3f6ffdc629a3b953cf94e
Abinayathirumaran <abishiva05@gmail.com>
2026-07-14 00:11:30 +0530

    fix: prepare routes for deployment

--- src/api/apiinstance.js
+++ src/api/apiinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL :"https://mycourse-1zbm.onrender.com"
+    baseURL :"https://mycourse-1zbm.onrender.com/api"
 });
 
 export default api;
